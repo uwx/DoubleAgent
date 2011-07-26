a8f014271e70efef778a905ad31ec4949785d15d
cinsoft <cinsoft@bf1daaaf-cdee-461b-8ea6-87a9be374999>
2011-07-26 04:45:58 +0000

    git-svn-id: https://svn.code.sf.net/p/doubleagent/code@408 bf1daaaf-cdee-461b-8ea6-87a9be374999

--- source/branches/Version 1.2 wip/Core/DaCore_p(x64).c
+++ source/branches/Version 1.2 wip/Core/DaCore_p(x64).c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Jul 11 13:09:53 2011
+/* at Fri Jul 22 01:21:39 2011
  */
 /* Compiler settings for Core\DaCore.odl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
