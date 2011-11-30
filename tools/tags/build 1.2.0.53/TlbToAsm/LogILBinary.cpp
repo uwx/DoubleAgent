b2f3f6d567649ccb3d3ea3838e87cbc98a4a2ece
cinsoft <cinsoft@bf1daaaf-cdee-461b-8ea6-87a9be374999>
2011-11-30 21:09:19 +0000

    Build 1.2.0.53 (Beta Release)
    git-svn-id: https://svn.code.sf.net/p/doubleagent/code@416 bf1daaaf-cdee-461b-8ea6-87a9be374999

---
+++ tools/tags/build 1.2.0.53/TlbToAsm/LogILBinary.cpp
@@ -0,0 +1,335 @@
+#include "stdafx.h"
+#include "LogILBinary.h"
+#include "LogAssembly.h"
+
+/////////////////////////////////////////////////////////////////////////////
+
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections;
+using namespace System::Collections::Generic;
+using namespace System::Reflection;
+using namespace System::Reflection::Emit;
+
+namespace DoubleAgent {
+namespace TlbToAsm {
+/////////////////////////////////////////////////////////////////////////////
+
+LogILBinary::LogILBinary (Module^ pSourceModule, UINT pLogLevel, int pLogIndent)
+:	ParseILBinary (pSourceModule),
+	mLogLevel (pLogLevel),
+	mLogIndent (pLogIndent)
+{}
+
+void LogILBinary::LogMethodBody (MethodBase^ pSourceMethod)
+{
+	try
+	{
+		MethodParseData^	lParseData = gcnew MethodParseData;
+
+		if	(GetMethodBody (pSourceMethod, lParseData))
+		{
+			LogMessage (mLogLevel, _T("%s.method %s %s (%s)"), _B(LogIndent()), _B(LogAssembly::MethodAttrsStr(pSourceMethod->Attributes)), _BM(pSourceMethod), _BMT(pSourceMethod));
+			LogMessage (mLogLevel, _T("%s{"), _B(LogIndent()));
+			mLogIndent++;
+			LogMessage (mLogLevel, _T("%s// Code size %d (0x%X)"), _B(LogIndent()), lParseData->mBinary->Length, lParseData->mBinary->Length);
+
+			ProcessMethodBody (lParseData);
+
+			mLogIndent--;
+			LogMessage (mLogLevel, _T("%s}"), _B(LogIndent()));
+		}
+	}
+	catch AnyExceptionDebug
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void LogILBinary::PutLocalVariables (Object^ pData)
+{
+	try
+	{
+		MethodParseData^	lData = safe_cast <MethodParseData^> (pData);
+		LocalVariableInfo^	lLocalVariable;
+
+		if	(
+				(lData->mMethodBody->LocalVariables)
+			&&	(lData->mMethodBody->LocalVariables->Count > 0)
+			)
+		{
+			String^	lInitStr = "";
+			int		lNdx;
+
+			if	(lData->mMethodBody->InitLocals)
+			{
+				lInitStr = " init";
+			}
+			LogMessage (mLogLevel, _T("%s.locals%s"), _B(LogIndent()), _B(lInitStr));
+
+			for	(lNdx = 0; lNdx < lData->mMethodBody->LocalVariables->Count; lNdx++)
+			{
+				lLocalVariable = lData->mMethodBody->LocalVariables [lNdx];
+				LogMessage (mLogLevel, _T("%s  %s V_%d"), _B(LogIndent()), _BT(lLocalVariable->LocalType), lNdx);
+			}
+		}
+	}
+	catch AnyExceptionDebug
+}
+
+bool LogILBinary::PutBodyException (Object^ pData, System::Reflection::Emit::OpCode & pOpCode, LPBYTE pOperand, int pOffset, LPBYTE pBinary)
+{
+	MethodParseData^	lData = safe_cast <MethodParseData^> (pData);
+
+	if	(lData->mTryEndAt->ContainsKey (pOffset))
+	{
+		for	(int lNdx = 0; lNdx < lData->mTryEndAt [pOffset]; lNdx++)
+		{
+			mLogIndent--;
+			LogMessage (mLogLevel, _T("%s}  // end .try"), _B(LogIndent()));
+		}
+	}
+	else
+	if	(lData->mCatchEndAt->ContainsKey (pOffset))
+	{
+		mLogIndent--;
+		LogMessage (mLogLevel, _T("%s}  // end handler"), _B(LogIndent()));
+	}
+
+	if	(lData->mCatchStartAt->ContainsKey (pOffset))
+	{
+		ExceptionHandlingClause^	lExceptionClause = lData->mCatchStartAt [pOffset];
+
+		if	((int)lExceptionClause->Flags & (int)ExceptionHandlingClauseOptions::Finally)
+		{
+			LogMessage (mLogLevel, _T("%sfinally"), _B(LogIndent()));
+		}
+		else
+		if	((int)lExceptionClause->Flags & (int)ExceptionHandlingClauseOptions::Filter)
+		{
+			LogMessage (mLogLevel, _T("%sfilter"), _B(LogIndent()));
+		}
+		else
+		if	((int)lExceptionClause->Flags & (int)ExceptionHandlingClauseOptions::Fault)
+		{
+			LogMessage (mLogLevel, _T("%scatch (...)"), _B(LogIndent()));
+		}
+		else
+		{
+			LogMessage (mLogLevel, _T("%scatch (%s)"), _B(LogIndent()), _BT(lExceptionClause->CatchType));
+		}
+		LogMessage (mLogLevel, _T("%s{"), _B(LogIndent()));
+		mLogIndent++;
+	}
+
+	if	(lData->mTryStartAt->ContainsKey (pOffset))
+	{
+		for	(int lNdx = 0; lNdx < lData->mTryStartAt [pOffset]; lNdx++)
+		{
+			LogMessage (mLogLevel, _T("%s.try"), _B(LogIndent()));
+			LogMessage (mLogLevel, _T("%s{"), _B(LogIndent()));
+			mLogIndent++;
+		}
+	}
+	return true;
+}
+
+bool LogILBinary::PutBodyOpCode (Object^ pData, System::Reflection::Emit::OpCode & pOpCode, LPBYTE pOperand, int pOffset, LPBYTE pBinary)
+{
+	try
+	{
+		TCHAR	lOpBytesStr [100];
+		TCHAR	lOperandStr [100];
+
+		memset (lOpBytesStr, 0, sizeof(lOpBytesStr));
+		memset (lOperandStr, 0, sizeof(lOperandStr));
+
+		if	(false)
+		{
+			TCHAR	lOpCodeStr [8];
+
+			if	(pBinary[pOffset] == 0xFE)
+			{
+				_stprintf (lOpCodeStr, _T("%2.2X%2.2X"), pBinary[pOffset], pBinary[pOffset+1]);
+			}
+			else
+			{
+				_stprintf (lOpCodeStr, _T("%2.2X  "), pBinary[pOffset]);
+			}
+
+			if	(OperandIsToken(pOpCode))
+			{
+				try
+				{
+					_stprintf (lOperandStr, _T("(%2.2X)%6.6X"), HIBYTE(HIWORD(*(PDWORD)pOperand)), (*(PDWORD)pOperand)&0x00FFFFFF);
+				}
+				catch AnyExceptionSilent
+			}
+			else
+			{
+				try
+				{
+					if	(OperandSize (pOpCode) == 8)
+					{
+						_stprintf (lOperandStr, _T("%16.16I64X"), *(PULONGLONG)pOperand);
+					}
+					else
+					if	(OperandSize (pOpCode) == 4)
+					{
+						_stprintf (lOperandStr, _T("%8.8X"), *(PDWORD)pOperand);
+					}
+					else
+					if	(OperandSize (pOpCode) == 2)
+					{
+						_stprintf (lOperandStr, _T("%4.4X"), *(PWORD)pOperand);
+					}
+					else
+					{
+						_stprintf (lOperandStr, _T("%2.2X"), *pOperand);
+					}
+				}
+				catch AnyExceptionSilent
+			}
+
+			_stprintf (lOpBytesStr, _T(" /* %s | %-16s */"), lOpCodeStr, lOperandStr);
+			memset (lOperandStr, 0, sizeof(lOperandStr));
+		}
+
+		switch (pOpCode.OperandType)
+		{
+			case OperandType::InlineType:
+			{
+				Type^ lType = GetTokenType (pOpCode, *(PDWORD)pOperand);
+				_stprintf (lOperandStr, _T("%s"), _BT(lType));
+			}	break;
+			case OperandType::InlineField:
+			{
+				FieldInfo^ lField = GetTokenField (pOpCode, *(PDWORD)pOperand);
+				_stprintf (lOperandStr, _T("%s.%s"), _BMT(lField), _BM(lField));
+			}	break;
+			case OperandType::InlineMethod:
+			{
+				MethodBase^ lMethod = GetTokenMethod (pOpCode, *(PDWORD)pOperand);
+				_stprintf (lOperandStr, _T("%s.%s"), _BMT(lMethod), _BM(lMethod));
+			}	break;
+			case OperandType::InlineI:
+			{
+				_stprintf (lOperandStr, _T("%d (0x%X)"), *(PDWORD)pOperand, *(PDWORD)pOperand);
+			}	break;
+			case OperandType::InlineI8:
+			{
+				_stprintf (lOperandStr, _T("%I64d (0x%I64X)"), *(PLONGLONG)pOperand, *(PLONGLONG)pOperand);
+			}	break;
+			case OperandType::InlineR:
+			{
+				_stprintf (lOperandStr, _T("%f (0x%I64X)"), *(double*)pOperand, *(PLONGLONG)pOperand);
+			}	break;
+			case OperandType::ShortInlineI:
+			{
+				_stprintf (lOperandStr, _T("%d (0x%X)"), *(PWORD)pOperand, *(PWORD)pOperand);
+			}	break;
+			case OperandType::ShortInlineR:
+			{
+				_stprintf (lOperandStr, _T("%f (0x%X)"), *(float*)pOperand, *(PDWORD)pOperand);
+			}	break;
+			case OperandType::InlineVar:
+			{
+				_stprintf (lOperandStr, _T("%d"), *(PWORD)pOperand);
+			}	break;
+			case OperandType::ShortInlineVar:
+			{
+				_stprintf (lOperandStr, _T("%d"), *pOperand);
+			}	break;
+			case OperandType::InlineBrTarget:
+			{
+				int	lTarget = pOffset + pOpCode.Size + OperandSize(pOpCode) + *(Int32*)pOperand;
+				_stprintf (lOperandStr, _T("IL_%4.4X"), lTarget);
+			}	break;
+			case OperandType::ShortInlineBrTarget:
+			{
+				int	lTarget = pOffset + pOpCode.Size + OperandSize(pOpCode) + *(SByte*)pOperand;
+				_stprintf (lOperandStr, _T("IL_%4.4X"), lTarget);
+			}	break;
+			default:
+			{
+				//_stprintf (lOperandStr, _T("{%s}"), _B(OperandTypeName(pOpCode)));
+			}
+		}
+
+		LogMessage (mLogLevel|LogHighVolume, _T("%sIL_%4.4X:%s %-10s %s"), _B(LogIndent()), pOffset, lOpBytesStr, _B(pOpCode.Name), lOperandStr);
+	}
+	catch AnyExceptionSilent
+
+	return true;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+#pragma page()
+/////////////////////////////////////////////////////////////////////////////
+
+String^ LogILBinary::OpCodeTypeName (System::Reflection::Emit::OpCode pOpCode)
+{
+	switch (pOpCode.OpCodeType)
+	{
+		case Emit::OpCodeType::Annotation:	return "Annotation"; break;
+		case Emit::OpCodeType::Macro:		return "Macro"; break;
+		case Emit::OpCodeType::Nternal:		return "Nternal"; break;
+		case Emit::OpCodeType::Objmodel:	return "Objmodel"; break;
+		case Emit::OpCodeType::Prefix:		return "Prefix"; break;
+		case Emit::OpCodeType::Primitive:	return "Primitive"; break;
+	}
+	return "<unknown>";
+}
+
+String^ LogILBinary::OpCodeFlowName (System::Reflection::Emit::OpCode pOpCode)
+{
+	switch (pOpCode.FlowControl)
+	{
+		case Emit::FlowControl::Branch:			return "Branch"; break;
+		case Emit::FlowControl::Break:			return "Break"; break;
+		case Emit::FlowControl::Call:			return "Call"; break;
+		case Emit::FlowControl::Cond_Branch:	return "Cond_Branch"; break;
+		case Emit::FlowControl::Meta:			return "Meta"; break;
+		case Emit::FlowControl::Next:			return "Next"; break;
+		case Emit::FlowControl::Phi:			return "Phi"; break;
+		case Emit::FlowControl::Return:			return "Return"; break;
+		case Emit::FlowControl::Throw:			return "Throw"; break;
+	}
+	return "<unknown>";
+}
+
+String^ LogILBinary::OperandTypeName (Emit::OpCode pOpCode)
+{
+	switch (pOpCode.OperandType)
+	{
+		case Emit::OperandType::InlineBrTarget:			return "InlineBrTarget"; break;
+		case Emit::OperandType::InlineField:			return "InlineField"; break;
+		case Emit::OperandType::InlineI:				return "InlineI"; break;
+		case Emit::OperandType::InlineI8:				return "InlineI8"; break;
+		case Emit::OperandType::InlineMethod:			return "InlineMethod"; break;
+		case Emit::OperandType::InlineNone:				return "InlineNone"; break;
+		case Emit::OperandType::InlinePhi:				return "InlinePhi"; break;
+		case Emit::OperandType::InlineR:				return "InlineR"; break;
+		case Emit::OperandType::InlineSig:				return "InlineSig"; break;
+		case Emit::OperandType::InlineString:			return "InlineString"; break;
+		case Emit::OperandType::InlineSwitch:			return "InlineSwitch"; break;
+		case Emit::OperandType::InlineTok:				return "InlineTok"; break;
+		case Emit::OperandType::InlineType:				return "InlineType"; break;
+		case Emit::OperandType::InlineVar:				return "InlineVar"; break;
+		case Emit::OperandType::ShortInlineBrTarget:	return "ShortInlineBrTarget"; break;
+		case Emit::OperandType::ShortInlineI:			return "ShortInlineI"; break;
+		case Emit::OperandType::ShortInlineR:			return "ShortInlineR"; break;
+		case Emit::OperandType::ShortInlineVar:			return "ShortInlineVar"; break;
+	}
+	return "<unknown>";
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+String^ LogILBinary::LogIndent ()
+{
+	return gcnew String(' ', mLogIndent*2);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+} // namespace TlbToAsm
+} // namespace DoubleAgent
---
+++ tools/tags/build 1.2.0.53/TlbToAsm/TlbToAsm.cpp
@@ -0,0 +1,581 @@
+#include "stdafx.h"
+#include "TlbToAsm.h"
+#include "FixupAssemblySpecific.h"
+#include "DaVersion.h"
+
+using namespace System::Runtime::InteropServices;
+using namespace System::Runtime::InteropServices::ComTypes;
+using namespace System::Reflection;
+using namespace System::Reflection::Emit;
+using namespace System::Security::Cryptography::X509Certificates;
+using namespace DoubleAgent;
+using namespace DoubleAgent::TlbToAsm;
+
+/////////////////////////////////////////////////////////////////////////////
+#pragma managed(push,off)
+#ifdef	_DEBUG
+#define _LOG_LEVEL_DEBUG	LogNormal
+#endif
+#define	_LOG_ROOT_PATH		_T("Software\\Cinnamon\\")
+#define	_LOG_SECTION_NAME	_T("TlbToAsm")
+#define _LOG_DEF_LOGNAME	_LOG_SECTION_NAME _T(".log")
+#include "LogAccess.inl"
+#include "Log.inl"
+#pragma comment(lib, "advapi32.lib")
+#pragma managed(pop)
+/////////////////////////////////////////////////////////////////////////////
+#pragma page()
+/////////////////////////////////////////////////////////////////////////////
+
+int main(array<System::String ^> ^args)
+{
+	bool								lRestartLog = true;
+	DoubleAgent::TlbToAsm::TlbToAsm^	lTlbToAsm;
+
+#ifdef	_DEBUG
+	LogStart (true);
+	lRestartLog = false;
+#endif
+
+	lTlbToAsm = gcnew DoubleAgent::TlbToAsm::TlbToAsm (lRestartLog);
+	lTlbToAsm->ProcessCmdLine (Environment::GetCommandLineArgs ());
+
+	try
+	{
+		GC::Collect ();
+	}
+	catch AnyExceptionSilent
+
+    LogStop (LogIfActive);
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+namespace DoubleAgent {
+namespace TlbToAsm {
+/////////////////////////////////////////////////////////////////////////////
+
+TlbToAsm::TlbToAsm (bool pRestartLog)
+:	mRestartLog (pRestartLog)
+{
+	mAssemblyVersion = gcnew System::Version (_DOUBLEAGENT_VERSION_STR);
+}
+
+int TlbToAsm::ProcessCmdLine (array <String^>^ pCmdArgs)
+{
+	int	lRet = 0;
+
+	try
+	{
+		int			lCmdArgNdx;
+		String^		lLastCmdArg;
+		Assembly^	lAssembly = nullptr;
+
+		for	(lCmdArgNdx = 1; lCmdArgNdx < pCmdArgs->Length; lCmdArgNdx++)
+		{
+			String^	lCmdOpt = CmdOpt (pCmdArgs, lCmdArgNdx);
+
+			if	(lCmdOpt == nullptr)
+			{
+				lLastCmdArg = pCmdArgs[lCmdArgNdx];
+				continue;
+			}
+
+			if	(String::Compare (lCmdOpt, "Convert", true) == 0)
+			{
+				try
+				{
+					String^	lSourcePath;
+					String^	lTargetPath;
+					bool	lLog = false;
+					bool	lShow = false;
+					bool	lSave = false;
+
+					while (lCmdOpt = CmdOpt (pCmdArgs, lCmdArgNdx+1))
+					{
+						if	(String::Compare (lCmdOpt, "Log", true) == 0)
+						{
+							lCmdArgNdx++;
+							lLog = true;
+							LogStart (mRestartLog);
+							mRestartLog = false;
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Show", true) == 0)
+						{
+							lCmdArgNdx++;
+							lShow = true;
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Save", true) == 0)
+						{
+							lCmdArgNdx++;
+							lSave = true;
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Version", true) == 0)
+						{
+							lCmdArgNdx++;
+							lCmdArgNdx++;
+							mAssemblyVersion = gcnew Version (CmdArg (pCmdArgs, lCmdArgNdx));
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Keyfile", true) == 0)
+						{
+							lCmdArgNdx++;
+							lCmdArgNdx++;
+							mStrongName = GetStrongName (CmdArg (pCmdArgs, lCmdArgNdx));
+						}
+						else
+						{
+							break;
+						}
+					}
+
+					if	(lSourcePath = CmdArg (pCmdArgs, lCmdArgNdx+1))
+					{
+						lCmdArgNdx++;
+						if	(lTargetPath = CmdArg (pCmdArgs, lCmdArgNdx+1))
+						{
+							lCmdArgNdx++;
+						}
+						else
+						{
+							lTargetPath = IO::Path::ChangeExtension (IO::Path::GetTempFileName(), ".dll");
+						}
+						if	(lSave)
+						{
+							lLastCmdArg = lTargetPath;
+						}
+
+						if	(lAssembly = ConvertTypeLib (lSourcePath, lTargetPath, lSave))
+						{
+							if	(lLog)
+							{
+								LogAssembly (lAssembly, false);
+							}
+							if	(lShow)
+							{
+								ShowAssembly (lAssembly);
+							}
+						}
+						else
+						{
+							lRet = 8;
+							break;
+						}
+					}
+				}
+				catch AnyExceptionDebug
+			}
+			else
+			if	(String::Compare (lCmdOpt, "Fix", true) == 0)
+			{
+				try
+				{
+					String^	lSourcePath;
+					String^	lTargetPath;
+					String^	lNamespace = gcnew String ("DoubleAgent");
+					bool	lLog = false;
+					bool	lShow = false;
+					bool	lSave = false;
+
+					while (lCmdOpt = CmdOpt (pCmdArgs, lCmdArgNdx+1))
+					{
+						if	(String::Compare (lCmdOpt, "Log", true) == 0)
+						{
+							lCmdArgNdx++;
+							lLog = true;
+							LogStart (mRestartLog);
+							mRestartLog = false;
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Show", true) == 0)
+						{
+							lCmdArgNdx++;
+							lShow = true;
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Save", true) == 0)
+						{
+							lCmdArgNdx++;
+							lSave = true;
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Namespace", true) == 0)
+						{
+							lCmdArgNdx++;
+							lNamespace = CmdArg (pCmdArgs, ++lCmdArgNdx);
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Version", true) == 0)
+						{
+							lCmdArgNdx++;
+							lCmdArgNdx++;
+							mAssemblyVersion = gcnew Version (CmdArg (pCmdArgs, lCmdArgNdx));
+						}
+						else
+						if	(String::Compare (lCmdOpt, "Keyfile", true) == 0)
+						{
+							lCmdArgNdx++;
+							lCmdArgNdx++;
+							mStrongName = GetStrongName (CmdArg (pCmdArgs, lCmdArgNdx));
+						}
+						else
+						{
+							break;
+						}
+					}
+
+					if	(lSourcePath = CmdArg (pCmdArgs, lCmdArgNdx+1))
+					{
+						lCmdArgNdx++;
+						if	(lTargetPath = CmdArg(pCmdArgs,lCmdArgNdx+1))
+						{
+							lCmdArgNdx++;
+						}
+						else
+						{
+							lTargetPath = lSourcePath;
+							lSourcePath = lLastCmdArg;
+						}
+						lLastCmdArg = lTargetPath;
+						if	(lSourcePath)
+						{
+							LogMessage (LogNormal, _T("Load %s"), _B(lSourcePath));
+							lAssembly = Assembly::LoadFrom (lSourcePath);
+						}
+
+						if	(lAssembly = FixAssembly (lAssembly, lTargetPath, lNamespace, lSave))
+						{
+							if	(lLog)
+							{
+								LogAssembly (lAssembly, false);
+							}
+							if	(lShow)
+							{
+								ShowAssembly (lAssembly);
+							}
+						}
+						else
+						{
+							lRet = 8;
+							break;
+						}
+					}
+				}
+				catch AnyExceptionDebug
+			}
+			else
+			if	(String::Compare (lCmdOpt, "Log", true) == 0)
+			{
+				try
+				{
+					bool	lLogCode = false;
+
+					if	(
+							(lCmdOpt = CmdOpt (pCmdArgs, lCmdArgNdx+1))
+						&&	(String::Compare (lCmdOpt, "Code", true) == 0)
+						)
+					{
+						lCmdArgNdx++;
+						lLogCode = true;
+					}
+					LogStart (mRestartLog);
+					mRestartLog = false;
+					lLastCmdArg = CmdArg(pCmdArgs,lCmdArgNdx+1) ? CmdArg(pCmdArgs,++lCmdArgNdx) : lLastCmdArg;
+					OutputAssembly (lLastCmdArg, true, lLogCode, false);
+				}
+				catch AnyExceptionDebug
+			}
+			else
+			if	(String::Compare (lCmdOpt, "Show", true) == 0)
+			{
+				try
+				{
+					lLastCmdArg = CmdArg(pCmdArgs,lCmdArgNdx+1) ? CmdArg(pCmdArgs,++lCmdArgNdx) : lLastCmdArg;
+					OutputAssembly (lLastCmdArg, false, false, true);
+				}
+				catch AnyExceptionDebug
+			}
+		}
+	}
+	catch AnyExceptionDebug
+
+	return lRet;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+String^ TlbToAsm::CmdOpt (array <String^>^ pCmdArgs, int pCmdArgNdx)
+{
+	if	(
+			(pCmdArgNdx < pCmdArgs->Length)
+		&&	(
+				(pCmdArgs[pCmdArgNdx][0] == '-')
+			||	(pCmdArgs[pCmdArgNdx][0] == '/')
+			)
+		)
+	{
+		return pCmdArgs[pCmdArgNdx]->Substring (1);
+	}
+	return nullptr;
+}
+
+String^ TlbToAsm::CmdArg (array <String^>^ pCmdArgs, int pCmdArgNdx)
+{
+	if	(
+			(pCmdArgNdx < pCmdArgs->Length)
+		&&	(pCmdArgs[pCmdArgNdx][0] != '-')
+		&&	(pCmdArgs[pCmdArgNdx][0] != '/')
+		)
+	{
+		return pCmdArgs[pCmdArgNdx];
+	}
+	return nullptr;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+#pragma page()
+/////////////////////////////////////////////////////////////////////////////
+
+AssemblyBuilder^ TlbToAsm::FixAssembly (Assembly^ pAssembly, String^ pAssemblyName, String^ pModuleName, bool pSaveAssembly)
+{
+	AssemblyBuilder^	lRet = nullptr;
+
+	try
+	{
+		AssemblyBuilder^	lAssemblyBuilder = nullptr;
+		Assembly^			lSavedAssembly;
+
+		pAssemblyName = pAssemblyName->Replace ("\\\\", "\\");
+
+		if	(String::IsNullOrEmpty (pModuleName))
+		{
+			Console::WriteLine ("Copying {0} to {1}", pAssembly->GetModules()[0]->FullyQualifiedName, pAssemblyName);
+		}
+		else
+		{
+			Console::WriteLine ("Copying {0} to {1} Namespace {2}", pAssembly->GetModules()[0]->FullyQualifiedName, pAssemblyName, pModuleName);
+		}
+
+		if	(!mCopyAssembly)
+		{
+			mCopyAssembly = gcnew CopyAssembly;
+			mCopyAssembly->mFixups = gcnew FixupAssemblySpecific (mCopyAssembly);
+		}
+
+		if	(lAssemblyBuilder = mCopyAssembly->MakeCopy (pAssembly, pAssemblyName, pModuleName, mAssemblyVersion, mStrongName))
+		{
+			try
+			{
+				mCopyAssembly->CopyTypes ();
+				mCopyAssembly->CopyMethodBodies ();
+			}
+			catch AnyExceptionDebug
+
+			if	(lAssemblyBuilder = mCopyAssembly->CreateCopy ())
+			{
+				MarkAssemblyVersion (lAssemblyBuilder);
+				if	(mStrongName)
+				{
+					MarkPrimaryAssembly (lAssemblyBuilder);
+				}
+
+				if	(pSaveAssembly)
+				{
+					LogMessage (LogNormal, _T("Save %s"), _B(pAssemblyName));
+					lAssemblyBuilder->Save (IO::Path::GetFileName (pAssemblyName));
+
+					try
+					{
+						if	(lSavedAssembly = Assembly::LoadFrom (pAssemblyName))
+						{
+							mCopyAssembly->mSavedAssemblies->Add (lSavedAssembly);
+							if	(mStrongName)
+							{
+								Console::WriteLine ("Saved {0} (strong named)", pAssemblyName);
+							}
+							else
+							{
+								Console::WriteLine ("Saved {0}", pAssemblyName);
+							}
+						}
+					}
+					catch AnyExceptionSilent
+				}
+				lRet = lAssemblyBuilder;
+			}
+		}
+	}
+	catch AnyExceptionDebug
+
+	try
+	{
+		GC::Collect ();
+	}
+	catch AnyExceptionSilent
+
+	return lRet;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+Assembly^ TlbToAsm::ConvertTypeLib (String^ pTlbFileName, String^ pAssemblyFileName, bool pSaveAssembly)
+{
+	Assembly^	lRet = nullptr;
+
+	try
+	{
+		AssemblyBuilder^	lAssemblyBuilder = nullptr;
+		TlbConverter^		lTlbConverter = gcnew TlbConverter;
+
+		lTlbConverter->mStdOleAssembly = Assembly::Load ("stdole, Version=7.0.3300.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
+		LogMessage (LogNormal, _T("Convert %s"), _B(pTlbFileName));
+		if	(lAssemblyBuilder = lTlbConverter->ConvertTypeLib (pTlbFileName, pAssemblyFileName, mStrongName))
+		{
+			if	(pSaveAssembly)
+			{
+				LogMessage (LogNormal, _T("Save %s"), _B(pAssemblyFileName));
+				lAssemblyBuilder->Save (IO::Path::GetFileName (pAssemblyFileName));
+			}
+			lRet = lAssemblyBuilder;
+		}
+	}
+	catch AnyExceptionDebug
+
+	return lRet;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+Assembly^ TlbToAsm::OutputAssembly (String^ pFileName, bool pLogAssembly, bool pLogCode, bool pShowAssembly)
+{
+	Assembly^	lAssembly = nullptr;
+
+	try
+	{
+		LogMessage (LogNormal, _T("Load %s"), _B(pFileName));
+		lAssembly = Assembly::LoadFrom (pFileName);
+
+		if	(pLogAssembly)
+		{
+			LogAssembly (lAssembly, pLogCode);
+		}
+		if	(pShowAssembly)
+		{
+			ShowAssembly (lAssembly);
+		}
+	}
+	catch AnyExceptionDebug
+
+	try
+	{
+		GC::Collect ();
+	}
+	catch AnyExceptionSilent
+
+	return lAssembly;
+}
+
+void TlbToAsm::LogAssembly (Assembly^ pAssembly, bool pLogCode)
+{
+	if	(pAssembly)
+	{
+		try
+		{
+			DoubleAgent::TlbToAsm::LogAssembly^	lLogAssembly = gcnew DoubleAgent::TlbToAsm::LogAssembly;
+
+			lLogAssembly->Log (pAssembly, pLogCode);
+		}
+		catch AnyExceptionDebug
+	}
+}
+
+void TlbToAsm::ShowAssembly (Assembly^ pAssembly)
+{
+	if	(pAssembly)
+	{
+		try
+		{
+			DoubleAgent::TlbToAsm::AssemblyDlg^	lAssemblyDlg = gcnew DoubleAgent::TlbToAsm::AssemblyDlg;
+
+			lAssemblyDlg->ShowAssembly (pAssembly);
+			lAssemblyDlg->ShowDialog ();
+		}
+		catch AnyExceptionDebug
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////
+#pragma page()
+/////////////////////////////////////////////////////////////////////////////
+
+StrongNameKeyPair^ TlbToAsm::GetStrongName (String^ pKeyFilePath)
+{
+	StrongNameKeyPair^	lStrongName = nullptr;
+
+	try
+	{
+		IO::FileStream^ lFile = gcnew IO::FileStream (pKeyFilePath, IO::FileMode::Open, IO::FileAccess::Read);
+		lStrongName = gcnew StrongNameKeyPair (lFile);
+		LogMessage (LogNormal, _T("StrongName [%s] [%d]"), _B(lFile->Name), lStrongName->PublicKey->Length);
+	}
+	catch AnyExceptionDebug
+
+	return lStrongName;
+}
+
+bool TlbToAsm::MarkAssemblyVersion (AssemblyBuilder^ pAssembly)
+{
+	bool	lRet = false;
+
+	mAssemblyVersion = gcnew Version (mAssemblyVersion->Major, Math::Max(mAssemblyVersion->Minor,0), Math::Max((int)mAssemblyVersion->MajorRevision,0), Math::Max((int)mAssemblyVersion->MinorRevision,0));
+
+	try
+	{
+		pAssembly->DefineVersionInfoResource (_DOUBLEAGENT_NAME, mAssemblyVersion->ToString(), _DOUBLEAGENT_COMPANY, _DOUBLEAGENT_COPYRIGHT, nullptr);
+	}
+	catch AnyExceptionDebug
+
+	try
+	{
+		array <Type^>^		lAttrArgTypes = gcnew array <Type^> (1);
+		array <Object^>^	lAttrArgValues = gcnew array <Object^> (1);
+
+		lAttrArgTypes[0] = System::String::typeid;
+		lAttrArgValues[0] = gcnew String (mAssemblyVersion->ToString());
+
+		pAssembly->SetCustomAttribute (gcnew CustomAttributeBuilder (AssemblyVersionAttribute::typeid->GetConstructor(lAttrArgTypes), lAttrArgValues));
+		lRet = true;
+	}
+	catch AnyExceptionSilent
+
+	return lRet;
+}
+
+bool TlbToAsm::MarkPrimaryAssembly (AssemblyBuilder^ pAssembly)
+{
+	bool	lRet = false;
+
+	try
+	{
+		array<Type^>^	lAttrArgTypes = gcnew array<Type^> (2);
+		array<Object^>^	lAttrArgValues = gcnew array<Object^> (2);
+
+		lAttrArgTypes [0] = Int32::typeid;
+		lAttrArgTypes [1] = Int32::typeid;
+		lAttrArgValues [0] = gcnew Int32(mAssemblyVersion->Major);
+		lAttrArgValues [1] = gcnew Int32(mAssemblyVersion->Minor);
+
+		pAssembly->SetCustomAttribute (gcnew CustomAttributeBuilder (PrimaryInteropAssemblyAttribute::typeid->GetConstructor(lAttrArgTypes), lAttrArgValues));
+		lRet = true;
+	}
+	catch AnyExceptionDebug
+
+	return lRet;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+} // namespace TlbToAsm
+} // namespace DoubleAgent
