8f3166dd7d6268956667f4a24c4e31ce62611e87
cinsoft <cinsoft@bf1daaaf-cdee-461b-8ea6-87a9be374999>
2012-01-25 16:16:24 +0000

    Minor documentation reorganization.
    git-svn-id: https://svn.code.sf.net/p/doubleagent/code@424 bf1daaaf-cdee-461b-8ea6-87a9be374999

--- source/branches/Version 1.2 wip/Include/DaServer_i.c
+++ source/branches/Version 1.2 wip/Include/DaServer_i.c
@@ -6,10 +6,10 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Jan 06 16:10:23 2012
+/* at Thu Jan 19 22:55:07 2012
  */
 /* Compiler settings for Server\DaServer.odl:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
